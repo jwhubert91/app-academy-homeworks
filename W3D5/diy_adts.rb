0222cf00fb2985302759d4005ff95bed4975c7f8
james w hubert <jwhubert91@gmail.com>
2019-10-11 00:39:12 -0400

    Add stack and queue classes with instance methods

---
+++ W3D5/diy_adts.rb
@@ -0,0 +1,51 @@
+# W3D5 homework - diy algorithmic data types
+
+class Stack
+    def initialize
+        @stack = Array.new
+    end
+
+    def push(el)
+        @stack << el
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end
+
+# p new_stack = Stack.new
+# p new_stack.push("1st el")
+# p new_stack.push("2nd el")
+# p new_stack.push("3rd el")
+# p new_stack.pop
+# p new_stack.peek
+
+class Queue
+    def initialize
+        @queue = Array.new
+    end
+
+    def enqueue(el)
+        @queue.unshift(el)
+    end
+
+    def dequeue
+        @queue.pop
+    end
+
+    def peek
+        @queue.last
+    end
+end
+
+# p new_stack = Stack.new
+# p new_stack.push("1st el")
+# p new_stack.push("2nd el")
+# p new_stack.push("3rd el")
+# p new_stack.pop
+# p new_stack.peek
